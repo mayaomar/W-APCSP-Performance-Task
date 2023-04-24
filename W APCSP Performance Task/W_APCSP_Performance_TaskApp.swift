6723309835a960f6d9c1c2fabd69afe7fd5443de
mayaomar <nina.omar247@gmail.com>
2023-04-24 18:13:14 -0500

    Initial Commit

---
+++ W APCSP Performance Task/W_APCSP_Performance_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  W_APCSP_Performance_TaskApp.swift
+//  W APCSP Performance Task
+//
+//  Created by Maya Omar24 on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct W_APCSP_Performance_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
